f55d7461b52bf4bda2e3bc41b4f15a07014dbe1b
Petunia Neta <Petunianeta@gmail.com>
2018-06-26 14:09:43 -0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  teamA = 0;
+  teamB = 0;
   constructor(public navCtrl: NavController) {
 
   }
-
+  onReset(){
+    this.teamA = 0;
+    this.teamB = 0;
+  }
+onClickA(){
+  this.teamA +=3;
+  console.log(this.teamA);
+}
+onTwoA(){
+  this.teamA +=2;
+  console.log(this.teamA); 
+}
+onThrowA(){
+  this.teamA +=1;
+  console.log(this.teamA);  
+}
+onClickB(){
+  this.teamB +=3;
+  console.log(this.teamB);
+}
+onTwoB(){
+  this.teamB +=2;
+  console.log(this.teamB); 
+}
+onThrowB(){
+  this.teamB +=1;
+  console.log(this.teamB);
+}
 }
